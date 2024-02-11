a48b2b28e2d3f510cb48f7bac2734ff35629176c
whitecity01 <kh081104@gmail.com>
2024-02-12 05:19:56 +0900

    feat: LoginNavigation 컴포넌트 추가

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,12 +1,14 @@
 import LoginHeader from "components/auth/login/LoginHeader";
 import "styles/auth/login/Login.scss";
 import LoginForm from "./LoginForm";
+import LoginNavigation from "./LoginNavigation";
 
 const Login = ({ myEmail }) => {
   return (
     <div className="login-container">
       <LoginHeader />
       <LoginForm myEmail={myEmail} />
+      <LoginNavigation />
     </div>
   );
 };

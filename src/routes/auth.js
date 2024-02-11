498c94fe0a41f33ddc4a32f3673be7f4df77ee23
whitecity01 <kh081104@gmail.com>
2024-02-11 22:23:19 +0900

    feat: auth route 추가

---
+++ src/routes/auth.js
@@ -0,0 +1,13 @@
+import LoginPage from "pages/auth/LoginPage";
+
+const auth = [
+  {
+    index: true,
+    element: <LoginPage />,
+  },
+  {
+    path: "register",
+  },
+];
+
+export default auth;

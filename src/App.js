8ee6e9c319190ffc9b42a92475fe03eb35539e17
whitecity01 <kh081104@gmail.com>
2024-02-11 22:23:46 +0900

    feat: RouterProvider 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { RouterProvider } from "react-router-dom";
+import router from "routes/root";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;

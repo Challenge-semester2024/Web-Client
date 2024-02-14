9f4553d8b78bdb14ca239db510b18414d07d7220
whitecity01 <kh081104@gmail.com>
2024-02-15 08:16:16 +0900

    feat: 카카오 api 추가

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,3 @@
 export const SERVER_IP = process.env.REACT_APP_SERVER_IP;
+export const KAKAO_REST_API = process.env.REACT_APP_KAKAO_REST_API;
+export const KAKAO_MAP_API = process.env.REACT_APP_KAKAO_MAP_API;

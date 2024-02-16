50b413431d51f6f1946250e606899fb32b50422b
whitecity01 <kh081104@gmail.com>
2024-02-16 23:08:25 +0900

    feat: 승인대기 이미지 추가 및 navigation btn 추가

--- src/components/auth/register/Wait.jsx
+++ src/components/auth/register/Wait.jsx
@@ -1,5 +1,15 @@
+import waitImg from "assets/wait.png";
+import { Link } from "react-router-dom";
+import "styles/auth/register/Wait.scss";
 const Wait = () => {
-  return <div>잠시 대기</div>;
+  return (
+    <div className="wait-container">
+      <img className="wait-image" src={waitImg} alt="wait" />
+      <Link to="/" className="login-navigation">
+        홈 화면으로 돌아가기
+      </Link>
+    </div>
+  );
 };
 
 export default Wait;

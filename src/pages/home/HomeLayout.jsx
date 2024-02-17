f48bee032b7138c1223ceeae95baac9ddc748d87
whitecity01 <kh081104@gmail.com>
2024-02-18 01:11:03 +0900

    feat: HomeSidebar 컴포넌트 추가

--- src/pages/home/HomeLayout.jsx
+++ src/pages/home/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import HomeFooter from "components/home/HomeFooter";
 import HomeHeader from "components/home/HomeHeader";
+import HomeSidebar from "components/home/HomeSidebar";
 import { Outlet } from "react-router-dom";
 import "styles/home/HomeLayout.scss";
 const HomeLayout = () => {
   return (
     <div className="homepage-container">
       <div className="homepage-body-container">
-        <div className="siderbar-container" />
+        <HomeSidebar />
         <div className="content-container">
           <HomeHeader />
           <Outlet />

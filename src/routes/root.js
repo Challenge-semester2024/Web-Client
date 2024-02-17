e652c5308e4139546f305e4beda39143729cc687
whitecity01 <kh081104@gmail.com>
2024-02-17 16:35:01 +0900

    feat: home route 추가

--- src/routes/root.js
+++ src/routes/root.js
@@ -2,13 +2,14 @@ import ErrorPage from "pages/ErrorPage";
 import RootLayout from "pages/RootLayout";
 import { createBrowserRouter } from "react-router-dom";
 import auth from "routes/auth";
+import home from "routes/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    children: [...auth],
+    children: [...auth, ...home],
   },
 ]);
 

85a041551e441ccfbd78709ced10b436517dca2f
whitecity01 <kh081104@gmail.com>
2024-02-17 16:33:43 +0900

    feat: HomeFooter 컴포넌트 추가

---
+++ src/components/home/HomeFooter.jsx
@@ -0,0 +1,21 @@
+const HomeFooter = () => {
+  return (
+    <>
+      <div className="site-info-container">
+        <span>(주) 어천메</span>
+      </div>
+      <div className="site-detail-info-container">
+        <span>주소: 영남광역시 기아구 컴공로 1004</span>
+        <span>대표: 정민주 | 사업자등록번호: 123-12-12345 </span>
+        <span>
+          개인정보 처리방침 | 이용약관 | 개인정보취급책임자: 책임 안져용~
+        </span>
+
+        <span>Kidz Heaven Makers v1.7.32 </span>
+        <span className="reserve-tag">@Heavenmaker. all rights reserved.</span>
+      </div>
+    </>
+  );
+};
+
+export default HomeFooter;

78debaa1f83afc6a3b562cfb994a932c0b67dbc8
whitecity01 <kh081104@gmail.com>
2024-02-22 02:05:11 +0900

    feat: GreetingPage 추가

---
+++ src/pages/home/facility/GreetingPage.jsx
@@ -0,0 +1,8 @@
+import Greeting from "components/home/facility/Greeting";
+import "styles/home/HomePage.scss";
+
+const GreetingPage = () => {
+  return <Greeting />;
+};
+
+export default GreetingPage;

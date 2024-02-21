6e6bc8574f964f24915a4a63185061963caba550
whitecity01 <kh081104@gmail.com>
2024-02-22 02:08:54 +0900

    feat: facility 라우트 추가

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,9 @@
 import HomeLayout from "pages/home/HomeLayout";
 import HomePage from "pages/home/HomePage";
+import FacilityPage from "pages/home/facility/FacilityPage";
+import GreetingPage from "pages/home/facility/GreetingPage";
+import IntroductionPage from "pages/home/facility/IntroductionPage";
+import RouteInfoPage from "pages/home/facility/RouteInfoPage";
 
 const home = [
   {
@@ -9,6 +13,24 @@ const home = [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "facility",
+        element: <FacilityPage />,
+        children: [
+          {
+            path: "greeting",
+            element: <GreetingPage />,
+          },
+          {
+            path: "introduction",
+            element: <IntroductionPage />,
+          },
+          {
+            path: "route-info",
+            element: <RouteInfoPage />,
+          },
+        ],
       },
     ],
   },

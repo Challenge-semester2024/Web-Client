418ff20fb0515a69d9c05d940ef377aa0c59bef0
whitecity01 <kh081104@gmail.com>
2024-03-12 01:53:21 +0900

    feat: VolunteerListTitle 컴포넌트추가

---
+++ src/components/home/volunteer/VolunteerListTitle.jsx
@@ -0,0 +1,12 @@
+import "styles/home/volunteer/VolunteerLayout.scss";
+
+const VolunteerListTitle = () => {
+  return (
+    <div className="volunteer-list-title-container">
+      <span className="volunteer-list-title">봉사 공고 목록</span>
+      <span className="volunteer-list-count">nnn개의 공고</span>
+    </div>
+  );
+};
+
+export default VolunteerListTitle;
